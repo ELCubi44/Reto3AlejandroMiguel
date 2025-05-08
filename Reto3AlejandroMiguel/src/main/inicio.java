23d92b8de0cbb409b1a3a400f67e903c8523b0dd
Alejillo478 <alejopv478@gmail.com>
2025-05-08 09:50:48 +0200

    actualización menú

--- Reto3AlejandroMiguel/src/main/inicio.java
+++ Reto3AlejandroMiguel/src/main/inicio.java
@@ -19,23 +19,57 @@ public class inicio {
 						.dimeEntero("1-Gestión de categorías\r\n2-Gestión de productos\r\n3-Gestión de clientes", sc);
 				switch (opcion2) {
 				case 1:
+
 					break;
 				case 2:
+
 					break;
 				case 3:
-					opcion3=funciones
+					opcion3 = funciones.dimeEntero("1-Alta de nuevos clientes\r\n2-Búsqueda por código", sc);
+					switch (opcion3) {
+					case 1:
+
+						break;
+					case 2:
+
+						break;
+					}
 					break;
 				}
 				break;
 			case 2:
 				opcion2 = funciones.dimeEntero("1-Listar productos por categoría\r\n2-Buscar productos", sc);
+				switch (opcion2) {
+				case 1:
+
+					break;
+				case 2:
+
+					break;
+				}
 				break;
 			case 3:
 				opcion2 = funciones.dimeEntero("1-Crear pedido\r\n2-Ver pedidos", sc);
+				switch (opcion2) {
+				case 1:
+
+					break;
+				case 2:
+
+					break;
+				}
 				break;
 
 			case 4:
 				opcion2 = funciones.dimeEntero("1-Bajo stock\r\n2-Pedidos por cliente\r\n3-Producto más vendido", sc);
+				switch (opcion2) {
+				case 1:
+
+					break;
+				case 2:
+
+					break;
+				}
 				break;
 			case 5:
 				System.out.println("Saliendo...");
