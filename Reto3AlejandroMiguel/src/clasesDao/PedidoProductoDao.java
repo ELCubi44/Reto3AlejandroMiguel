16f81f77077885c01c29b945ac2a9331011d4f65
ELCubi44 <migue@GalaxyBook>
2025-05-13 08:16:59 +0200

    pedido producto dao inserta

--- Reto3AlejandroMiguel/src/clasesDao/PedidoProductoDao.java
+++ Reto3AlejandroMiguel/src/clasesDao/PedidoProductoDao.java
@@ -1,7 +1,30 @@
  package clasesDao;
 
+import java.sql.*;
+import clases.*;
+
 public class PedidoProductoDao {
 
+	public static void inserta (PedidoProducto pedido) {
+		try {
+			Connection con = Conexion.abreConexion();
+			PreparedStatement pst = con.prepareStatement("INSERT INTO pedido ('idpedido','idproducto','unidades','precio') VALUES (?,?,?,?);", Statement.RETURN_GENERATED_KEYS);
+			pst.setInt(1,pedido.getPedido().getIdPedido());
+			pst.setDouble(2, pedido.getProducto().getIdProducto());
+			pst.setInt(3, pedido.getUnidades());
+			pst.setDouble(4, pedido.getPrecio());
+			pst.executeUpdate();
+			ResultSet rs = pst.getGeneratedKeys();
+				if (rs.next()) {
+					pedido.setIdPedidoProducto  (rs.getInt(1));
+				}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			Conexion.cierraConexion();
+		}
 	
+	}
 	
 }
