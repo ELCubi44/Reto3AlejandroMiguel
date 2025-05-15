26ebf2b72bef8f004de1a16c354c523e5f37ee87
ELCubi44 <migue@GalaxyBook>
2025-05-15 09:40:18 +0200

    cambio a funcionesMenu

--- Reto3AlejandroMiguel/src/main/inicio.java
+++ Reto3AlejandroMiguel/src/main/inicio.java
@@ -10,7 +10,7 @@ public class inicio {
 		int opcion = 0;
 		do {
 			opcion = funciones.dimeEntero(
-					"Menú:\r\n1-Mantenimientos\r\n2-Catálogo de productos\r\n3-Pedidos\r\n4-Informes\r\n5-Salir", sc);
+					"Menï¿½:\r\n1-Mantenimientos\r\n2-Catï¿½logo de productos\r\n3-Pedidos\r\n4-Informes\r\n5-Salir", sc);
 			switch (opcion) {
 			case 1:
 				submenu1(sc);
@@ -28,7 +28,7 @@ public class inicio {
 				System.out.println("Saliendo...");
 				break;
 			default:
-				System.out.println("Opción no válida");
+				System.out.println("Opciï¿½n no vï¿½lida");
 				break;
 			}
 		} while (opcion != 5);
@@ -39,13 +39,13 @@ public class inicio {
 		int opcion = 0;
 		do {
 			opcion = funciones.dimeEntero(
-					"1-Gestión de categorías\r\n2-Gestión de productos\r\n3-Gestión de clientes\r\n4-Volver", sc);
+					"1-Gestiï¿½n de categorï¿½as\r\n2-Gestiï¿½n de productos\r\n3-Gestiï¿½n de clientes\r\n4-Volver", sc);
 			switch (opcion) {
 			case 1:
-				funciones.gestionCat(sc);
+				FuncionesMenu.gestionCat(sc);
 				break;
 			case 2:
-				funciones.gestionPro(sc);
+				FuncionesMenu.gestionPro(sc);
 				break;
 			case 3:
 				submenu11(sc);
@@ -54,7 +54,7 @@ public class inicio {
 				System.out.println("Volviendo...");
 				break;
 			default:
-				System.out.println("Opción no válida");
+				System.out.println("Opciï¿½n no vï¿½lida");
 				break;
 			}
 		} while (opcion != 4);
@@ -63,19 +63,19 @@ public class inicio {
 	public static void submenu11(Scanner sc) {
 		int opcion = 0;
 		do {
-			opcion = funciones.dimeEntero("1-Alta de nuevos clientes\r\n2-Búsqueda por código\r\n3-Volver", sc);
+			opcion = funciones.dimeEntero("1-Alta de nuevos clientes\r\n2-Bï¿½squeda por cï¿½digo\r\n3-Volver", sc);
 			switch (opcion) {
 			case 1:
-				funciones.altaClientes(sc);
+				FuncionesMenu.altaClientes(sc);
 				break;
 			case 2:
-				funciones.busCod(sc);
+				FuncionesMenu.busCod(sc);
 				break;
 			case 3:
 				System.out.println("Volviendo...");
 				break;
 			default:
-				System.out.println("Opción no válida");
+				System.out.println("Opciï¿½n no vï¿½lida");
 				break;
 			}
 		} while (opcion != 3);
@@ -84,19 +84,19 @@ public class inicio {
 	public static void submenu2(Scanner sc) {
 		int opcion = 0;
 		do {
-			opcion = funciones.dimeEntero("1-Listar productos por categoría\r\n2-Buscar productos\r\n3-Volver", sc);
+			opcion = funciones.dimeEntero("1-Listar productos por categorï¿½a\r\n2-Buscar productos\r\n3-Volver", sc);
 			switch (opcion) {
 			case 1:
-				funciones.listarProductos(sc);
+				FuncionesMenu.listarProductos(sc);
 				break;
 			case 2:
-				funciones.buscarProd(sc);
+				FuncionesMenu.buscarProd(sc);
 				break;
 			case 3:
 				System.out.println("Volviendo...");
 				break;
 			default:
-				System.out.println("Opción no válida");
+				System.out.println("Opciï¿½n no vï¿½lida");
 				break;
 			}
 		} while (opcion != 3);
@@ -108,16 +108,16 @@ public class inicio {
 			opcion = funciones.dimeEntero("1-Crear pedido\r\n2-Ver pedidos\r\n3-Volver", sc);
 			switch (opcion) {
 			case 1:
-				funciones.crearPedido(sc);
+				FuncionesMenu.crearPedido(sc);
 				break;
 			case 2:
-				funciones.verPedidos();
+				FuncionesMenu.verPedidos();
 				break;
 			case 3:
 				System.out.println("Volviendo...");
 				break;
 			default:
-				System.out.println("Opción no válida");
+				System.out.println("Opciï¿½n no vï¿½lida");
 				break;
 			}
 		} while (opcion != 3);
@@ -126,23 +126,23 @@ public class inicio {
 	public static void submenu4(Scanner sc) {
 		int opcion = 0;
 		do {
-			opcion = funciones.dimeEntero("1-Bajo stock\r\n2-Pedidos por cliente\r\n3-Producto más vendido\r\n4-Volver",
+			opcion = funciones.dimeEntero("1-Bajo stock\r\n2-Pedidos por cliente\r\n3-Producto mï¿½s vendido\r\n4-Volver",
 					sc);
 			switch (opcion) {
 			case 1:
-				funciones.bajoStock(sc);
+				FuncionesMenu.bajoStock(sc);
 				break;
 			case 2:
-				funciones.pedidosCliente(sc);
+				FuncionesMenu.pedidosCliente(sc);
 				break;
 			case 3:
-				funciones.productoMvendido();
+				FuncionesMenu.productoMvendido();
 				break;
 			case 4:
 				System.out.println("Volviendo...");
 				break;
 			default:
-				System.out.println("Opción no válida");
+				System.out.println("Opciï¿½n no vï¿½lida");
 				break;
 			}
 		} while (opcion != 4);
