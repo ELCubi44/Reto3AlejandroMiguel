ac86fb89940cb903aae43a94ec814aa866d8abce
ELCubi44 <migue@GalaxyBook>
2025-05-19 13:56:32 +0200

    cate

--- Reto3AlejandroMiguel/src/clasesDao/CategoriaDao.java
+++ Reto3AlejandroMiguel/src/clasesDao/CategoriaDao.java
@@ -18,50 +18,50 @@ import util.Conexion;
 public class CategoriaDao {
 	/**
 	 * Metodo para listar las categorias
+	 * 
 	 * @return una lista con las categorias de la base de datos insertada
 	 */
-	public static List<Categoria> lista () {
-		List <Categoria> categorias = new ArrayList <Categoria>();
+	public static List<Categoria> lista() {
+		List<Categoria> categorias = new ArrayList<Categoria>();
 		try {
 			Connection con = util.Conexion.abreConexion();
 			PreparedStatement pst = con.prepareStatement("SELECT idcategoria,nombre FROM proyecto3ev.categorias;");
 			ResultSet rs = pst.executeQuery();
 			while (rs.next()) {
-				categorias.add(new Categoria (rs.getInt("idcategoria"),rs.getString("nombre")));
+				categorias.add(new Categoria(rs.getInt("idcategoria"), rs.getString("nombre")));
 			}
 			rs.close();
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			Conexion.cierraConexion();
 		}
 		return categorias;
 	}
+
 	/**
 	 * Sirve para insertar una categoria nueva en la base de datos
+	 * 
 	 * @param categoria nos pasan la categoria creada que queremos añadir
 	 */
-	public static void inserta (Categoria categoria) {
+	public static void inserta(Categoria categoria) {
 		try {
 			Connection con = util.Conexion.abreConexion();
-<<<<<<< HEAD
-			PreparedStatement pst = con.prepareStatement("INSERT INTO categorias (nombre) VALUES (?);", Statement.RETURN_GENERATED_KEYS);
-=======
-			PreparedStatement pst = con.prepareStatement("INSERT INTO categoria ('nombre') VALUES ?;", Statement.RETURN_GENERATED_KEYS);
->>>>>>> branch 'main' of https://github.com/ELCubi44/Reto3AlejandroMiguel.git
-			pst.setString(1,categoria.getNombre());
+
+			PreparedStatement pst = con.prepareStatement("INSERT INTO categorias (nombre) VALUES (?);",
+					Statement.RETURN_GENERATED_KEYS);
+
+			pst.setString(1, categoria.getNombre());
 			pst.executeUpdate();
 			ResultSet rs = pst.getGeneratedKeys();
-				if (rs.next()) {
-					categoria.setIdCategoria  (rs.getInt(1));
-				}
+			if (rs.next()) {
+				categoria.setIdCategoria(rs.getInt(1));
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			Conexion.cierraConexion();
 		}
-	
+
 	}
 }
